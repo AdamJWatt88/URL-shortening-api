e9d8e1ec634cc27860a77640086fa3ee4cd30f62
Adam <adamjwatt88@gmail.com>
2020-07-06 16:28:41 -0700

    CSS mostly done.media queries need fixing

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
+// add "Please add a link" text to small tag in form when user inputs nothing or incorrect input
+// copy button in user's links needs to change to copied
+
+const linksContainer = document.getElementById('shortened-links-container');
 const toggle = document.getElementById('toggle');
+
+
+
+
+
+// example of links html to load dynamically
+
+        // <li>
+        //   <span id="input-link" class="input-link"
+        //     >https://www.google.com/</span
+        //   >
+        //   <span id="shortened-link" class="shortened-link">google.com</span>
+        //   <button id="copy-btn" class="btn copy-btn">Copy</button>
+        // </li>
 
 
 
