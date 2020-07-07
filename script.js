450afdcbfadd03805e9fcc2bf766b1144e57476b
Adam <adamjwatt88@gmail.com>
2020-07-07 16:03:00 -0700

    progress

--- script.js
+++ script.js
@@ -1,26 +1,64 @@
 // add "Please add a link" text to small tag in form when user inputs nothing or incorrect input
 // copy button in user's links needs to change to copied
+// make POST request in form of users URL
+// then make a GET request with the users shortened URL
 
 const linksContainer = document.getElementById('shortened-links-container');
 const toggle = document.getElementById('toggle');
+const urlInput = document.getElementById('url-input');
+const shortenBtn = document.getElementById('shorten-btn');
+
+let hashArr = []
 
 
+function readLink() {
+    let link = {
+        url: urlInput.value
+    };
+    return link;
+}
 
+function update(json) {
+    if (hashArr.indexOf(json.hashid) === -1) {
+      linksContainer.innerHTML += `<div class="result">
+      <div class="normal-url">
+        <a href="${json.url}" target="_blank">${json.url}</a>
+      </div>
+      <div class="shortened-url">
+        <div id="short-url"><a href="https://rel.ink/${json.hashid}" target="_blank" id="url">https://rel.ink/${json.hashid}</a></div>
+        <button class="copy btn">Copy</button>
+      </div>`;
+    } else {
+      return;
+    }
+  }
 
-
-// example of links html to load dynamically
-
-        // <li>
-        //   <span id="input-link" class="input-link"
-        //     >https://www.google.com/</span
-        //   >
-        //   <span id="shortened-link" class="shortened-link">google.com</span>
-        //   <button id="copy-btn" class="btn copy-btn">Copy</button>
-        // </li>
-
-
+async function getLink(data) {
+    const URL = `https://rel.ink/api/links/`;
+    const fetchResult = fetch(URL, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify(data)
+    }) 
+    const response = await fetchResult
+    if (response.ok) {
+        const jsonData = await response.json();
+        update(jsonData);
+        hashArr.push(jsonData.hashid);
+        console.log(jsonData)
+    }
+}
 
 
 toggle.addEventListener('click', () => {
     document.querySelector('header nav').classList.toggle('show')
+})
+
+
+shortenBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    let data = readLink();
+    getLink(data)
 })
