33eab7ebafa4b9eac74fa3c7d68da14bba805390
Adam <adamjwatt88@gmail.com>
2020-07-13 14:03:15 -0700

    progress

--- script.js
+++ script.js
@@ -12,53 +12,56 @@ let hashArr = []
 
 
 function readLink() {
-    let link = {
-        url: urlInput.value
-    };
-    return link;
+  let link = {
+    url: urlInput.value
+  };
+  return link;
 }
 
 function update(json) {
-    if (hashArr.indexOf(json.hashid) === -1) {
-      linksContainer.innerHTML += `<div class="result">
+  if (hashArr.indexOf(json.hashid) === -1) {
+    linksContainer.innerHTML +=
+    `<div class="result">
       <div class="normal-url">
         <a href="${json.url}" target="_blank">${json.url}</a>
       </div>
       <div class="shortened-url">
-        <div id="short-url"><a href="https://rel.ink/${json.hashid}" target="_blank" id="url">https://rel.ink/${json.hashid}</a></div>
-        <button class="copy btn">Copy</button>
-      </div>`;
-    } else {
-      return;
-    }
+        <a href="https://rel.ink/${json.hashid}" target="_blank" id="url">https://rel.ink/${json.hashid}</a>
+      </div>
+      <button class="copy btn">Copy</button>
+     </div>`;
+  } else {
+    return;
   }
+}
+
 
 async function getLink(data) {
-    const URL = `https://rel.ink/api/links/`;
-    const fetchResult = fetch(URL, {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        }, 
-        body: JSON.stringify(data)
-    }) 
-    const response = await fetchResult
-    if (response.ok) {
-        const jsonData = await response.json();
-        update(jsonData);
-        hashArr.push(jsonData.hashid);
-        console.log(jsonData)
-    }
+  const URL = `https://rel.ink/api/links/`;
+  const fetchResult = fetch(URL, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  const response = await fetchResult
+  if (response.ok) {
+    const jsonData = await response.json();
+    update(jsonData);
+    hashArr.push(jsonData.hashid);
+    console.log(jsonData)
+  }
 }
 
 
 toggle.addEventListener('click', () => {
-    document.querySelector('header nav').classList.toggle('show')
+  document.querySelector('header nav').classList.toggle('show')
 })
 
 
 shortenBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    let data = readLink();
-    getLink(data)
+  e.preventDefault();
+  let data = readLink();
+  getLink(data)
 })
